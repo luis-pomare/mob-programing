8427cb2f83d0361a56d6dcc579828095d0ee38ef
unknown <luispomare92@gmail.com>
2022-07-14 09:39:27 -0500

    Mob programming 7 14 2022

--- linked-lists-next-step-luis-pomare/challenge.js
+++ linked-lists-next-step-luis-pomare/challenge.js
@@ -43,26 +43,35 @@ class LinkedList {
     return value;
   }
 
-  add_at(index, item) {
-    const node = this.get_node(index);
-    let current = this.head;
+  addAt(index, item) {
     let newNode = new Node(item, null);
-
-    if (index === 0) {
+    if (index > 0) {
+      const node = this.get_node(index - 1);
+      newNode.next_node = node.next_node;
+      node.next_node = newNode;
+    } else {
+      newNode.next_node = this.head;
       this.head = newNode;
-      newNode.next_node = current;
     }
   }
 
-  remove(index) {}
+  remove(index) {
+    const node = this.get_node(index);
+    if (index > 0) {
+      const prev = this.get_node(index - 1);
+      prev.next_node = node.next_node;
+    } else {
+      this.head = node.next_node;
+    }
+  }
 }
 
 const list = new LinkedList();
 
-list.add(3); // 3 1 5 15
-list.add(5);
+list.add(3); // 3 5 15
+list.add(5); // 7
 list.add(15);
-list.add_at(1);
+list.addAt(1, 7);
 console.log(list.get(0));
 console.log(list.get(2));
 
