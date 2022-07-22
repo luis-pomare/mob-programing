6a05708c6b98b137bc7d9baba8658cb2746a2632
unknown <luispomare92@gmail.com>
2022-07-22 09:18:35 -0500

    Mob programming 7 22 2022

--- binary-search-luis-pomare/challenge.js
+++ binary-search-luis-pomare/challenge.js
@@ -1,10 +1,20 @@
-module.exports = function sqrt(number) {
-  return sqrt_recursive(number, 0, number)
+function sqrt(number) {
+  return sqrt_recursive(number, 0, number);
 }
 
 function sqrt_recursive(number, min_interval, max_interval) {
   // write your code here
+  let middle = Math.floor((min_interval + max_interval) / 2);
+  if (middle * middle === number) {
+    return middle;
+  }
+  if (middle * middle < number) {
+    return sqrt_recursive(number, middle + 1, max_interval);
+  }
+  return sqrt_recursive(number, min_interval, middle - 1);
 }
 
-//console.log(sqrt(25))
-//console.log(sqrt(7056))
+console.log(sqrt(25));
+console.log(sqrt(7056));
+
+module.exports = sqrt;
